e587ea20d6102b97fd0f1eb662a57bdc0783c150
Givaona <giovanabanana0@gmail.com>
2022-04-14 17:34:45 -0300

    Js arrumado

--- apod.js
+++ apod.js
@@ -12,8 +12,8 @@ $("#submit").click(function (event){
     const date = $("#date").val();
     
     $.ajax({
-        // url:"https://api.nasa.gov/planetary/apod?api_key=dhfvOTyEAbEUszxxFC9be1YJUt8t4alLqoCxVM7P&date=${date}",
-        url:"https://api.nasa.gov/planetary/apod?api_key=dhfvOTyEAbEUszxxFC9be1YJUt8t4alLqoCxVM7P&date=2003-02-23",
+        url:`https://api.nasa.gov/planetary/apod?api_key=dhfvOTyEAbEUszxxFC9be1YJUt8t4alLqoCxVM7P&date=${date}`,
+        // url:"https://api.nasa.gov/planetary/apod?api_key=dhfvOTyEAbEUszxxFC9be1YJUt8t4alLqoCxVM7P&date=2003-02-23",
         success: function (response) {
           $("#dateTitle").text(response.date);
           $("#title").text(response.title)
